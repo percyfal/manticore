18ed716fc501835f341f9c02e2632b97fdc59011
Per Unneberg <per.unneberg@scilifelab.se>
2019-03-14 22:26:27 +0100

    Improve logging

--- exec/popgenome_stats.R
+++ exec/popgenome_stats.R
@@ -7,7 +7,6 @@
 ## Description:
 ## Calculate genome statistics for a vcf file with PopGenome
 ##
-
 library("getopt")
 library("PopGenome")
 library("manticore")
@@ -59,7 +58,10 @@ if (opt$cpus > 1) {
 }
 
 # Get length of scaffold
-ln <- as.numeric(gsub(">", "", gsub(".*length=", "", system(paste0("bcftools view -h ", opt$vcf, " | grep \"ID=", opt$scaffold, ",\""), intern=TRUE))))
+ln <- as.numeric(gsub(".*length=(\\d+).*", "\\1", system(paste0("bcftools view -h ", opt$vcf, " | grep \"ID=", opt$scaffold, ",\""), intern=TRUE)))
+message("Inferred scaffold length: ", ln)
+if (is.na(ln))
+    warning("cannot infer scaffold length")
 
 # Get samples present in vcf
 samples <- system(paste0("bcftools query -l ", opt$vcf), intern=TRUE)
@@ -86,15 +88,17 @@ load_data <- function(scaffold) {
     }}
     ## Get length from vcf
     message("Opening vcf file ", opt$vcf)
-    sink("/dev/null")
     vcf <- tryCatch({
+        sink("/dev/null")
         vcf <-readVCF(opt$vcf, 1000, frompos = 1, topos = ln,
                       tid = as.character(scaffold), gffpath = gff_file)
+        sink()
     }, error = function(err) {
-        message("Scaffold not in vcf; returning NA")
+        message(paste0("readVCF failed for scaffold ", scaffold, " returning NA"))
         return (NA)
     })
-    sink()
+
+    message("read vcf: ", opt$vcf)
     if (inherits(vcf, "GENOME")) {
         vcf@region.names <- as.character(scaffold)
         vcf <- set.populations(vcf, populations.list, diploid = TRUE)

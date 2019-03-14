64ffc8608300a21c64440b81613b6a70f1c00148
Per Unneberg <per.unneberg@scilifelab.se>
2019-03-14 21:24:37 +0100

    Add popoolation file

---
+++ R/popoolation.R
@@ -0,0 +1,11 @@
+##' readVarianceSliding
+##'
+##' Read VarianceSliding results from popoolation
+##'
+##' @title
+##' @return
+##' @author Per Unneberg
+readVarianceSliding <- function(measure="pi") {
+    measure <- match.arg(measure, c("pi", ""))
+
+}

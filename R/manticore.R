3d5c882987ed0b9db868b7b31a9600cb4246f332
Per Unneberg <per.unneberg@scilifelab.se>
2019-03-18 14:29:34 +0100

    Add main package file

---
+++ R/manticore.R
@@ -0,0 +1,15 @@
+#' manticore: A package for analyses of non-model organism data
+#'
+#' The manticore package provides functionality for working with non-model organism data
+#'
+#'
+#' @section manticore functions:
+#'
+#' gplot
+#'
+#' @docType package
+#' @name manticore
+#'
+#' @import methods
+#'
+NULL
